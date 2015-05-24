fa5c9eabbd1ccd237e49a05363a3e17e86558f18
root <root@droplet1.midconcept.com>
2015-05-24 03:31:33 -0400

    all

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>My Portfolio</title>
+
+	<!-- <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
+	<link href="/assets/css/style.css" rel="stylesheet" type="text/css">
+
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+	<!--  <script src="/assets/js/bootstrap.min.js"></script> -->
+	<script src="/assets/js/custom.js"></script>
+
+	<style type="text/css">
+	h4{
+		display: inline-block;
+	}
+		</style>
+	
+</head>
+<body>
+	<div id= "header">
+		<h4 class = "left">Home</h4>
+		<h4 class = "right"><a href="/logout">logout</h4>
+	</div>
+	
---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Rui Wang's work portfolio</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- <a class="navbar-brand" href="#">Start Bootstrap</a> -->
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <a href="" data-behavior="ajax" class="logo">
+                        <img width="58" style="float:left" height="58" src="../assets/images/rui.jpg" alt="Rui Wang">
+                </a>
+
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <!-- <li>
+                        <a href="#">Services</a>
+                    </li> -->
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Image Background Page Header -->
+    <!-- Note: The background image is set within the business-casual.css file. -->
+    <header class="business-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                    <h1 class="tagline">Rui Wang's work portfolio</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-4 item">
+                <a  class="align-center" href="#home">
+                    <img class="img-circle img-responsive img-center" src="../assets/images/user_dashboard.jpg" alt="">
+                    <div>
+                        <p class="text-center">User can login and post messages</p> 
+
+                        
+                    </div>
+                </a>
+                <p class="text-center" style="margin-top:20px;"><a href="http://www.midconcept.com:81/">User DashBoard (PHP)</a></p>
+            </div>
+             <div class="col-sm-4 item">
+                <a  class="align-center" href="#home">
+                    <img class="img-circle img-responsive img-center" src="../assets/images/e_commerce.jpg" alt="">
+                    <div>
+                        <p class="text-center">ECommerce group project to sell boots</p> 
+
+                        
+                    </div>
+                </a>
+                <p class="text-center" style="margin-top:20px;"><a href="http://www.midconcept.com:82/">E-Commerce (PHP Codeigniter)</a></p>
+            </div>
+            <div class="col-sm-4 item">
+                <a  class="align-center" href="#home">
+                    <img class="img-circle img-responsive img-center" src="../assets/images/mychat.jpg" alt="">
+                    <div>
+                        <p class="text-center">MEAN Stack chatting application</p> 
+
+                        
+                    </div>
+                </a>
+                <p class="text-center" style="margin-top:20px;"><a href="http://www.midconcept.com:3000/">myChat (MEAN Stack)</a></p>
+            </div>
+
+        </div>
+     
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Rui Wang@2015</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
